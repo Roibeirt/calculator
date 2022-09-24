9f1e79bdc6edb59b043f1648575330dff256a1e4
Bobby Aston <bobby.j.aston@gmail.com>
2022-09-24 16:30:56 +0900

    Adding more complex logic to calculator, will keep simple version as main branch

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function multiply(value1, value2){
 }
 
 //declare variable to hold current operator
-let operator = "";
+//let operator = "";
 
 //find operator buttons in DOM, declare them to variables
 //assign event listener to buttons to change currently selected operator
@@ -71,28 +71,47 @@ let newInput = false;
 //declare variables to hold values
 let currentValue = null;
 let previousValue = null;
+let newValue = null;
 
-//
-function assignOperator(operator){
+//function assigns operator values and calls operate function 
+function assignOperator(operator, equals){
 
+    //if there is no current operator, there is no second value so operate is not called
     if (currentOperator == null){
         currentValue = Number(calcScreen.value);
         currentOperator = operator;
+        operated = true;
     }
     else{
         //only change previous input if input changes, otherwise repeated operations will not use original value
         if (newInput == true){
-        previousValue = currentValue;
+            previousValue = currentValue;
+            newInput= false;
+            sameOperation = false
+        }
+        //if the operator has changed, the operation is not repeating
+        else if(operator != currentOperator){
+            sameOperation = false;
+        }
+        //From the second time an operation is run, the value which is operating should remain the same
+        else if(equals == true && sameOperation == false){
+            previousValue = currentValue;
+            sameOperation = true;
         }
         currentValue = Number(calcScreen.value);
         previousOperator = currentOperator;
         currentOperator = operator;
-        operate();
+        //because user has pressed two operator buttons, the expected function of the calculator is to operate using the value of the first button pressed
+        if(currentOperator == previousOperator && equals !=true){
+            operated = true;
+            return;
+        }
+        else if(equals != true) operate(previousOperator);
+        else operate(currentOperator);
     }
-    operated = true;
 }
 
-function operate(){
+function operate(operator){
 
     //if someone presses equals after the first operator, previous value won't be assigned. This makes sure it is assigned.
     if(previousValue == null){
@@ -100,38 +119,57 @@ function operate(){
         currentValue = Number(calcScreen.value);
     }
 
-    if (currentOperator == "divide"){
-        currentValue = divide(previousValue, currentValue);
+    if (operator == "divide"){
+        //if we are repeating the operation, it is expected that the new value will be divided by the old one.
+        //divide is the only operation where it is necessary to make this distinction
+        if(sameOperation == true){
+            calcScreen.value = divide(currentValue, previousValue);
+        }
+        else calcScreen.value = divide(previousValue, currentValue);
     }
-    else if (currentOperator == "multiply"){
-        currentValue = multiply(previousValue, currentValue);
+    else if (operator == "multiply"){
+        calcScreen.value = multiply(previousValue, currentValue);
     }
-    else if (currentOperator == "minus"){
-        currentValue = subtract(previousValue, currentValue);
+    else if (operator == "minus"){
+        calcScreen.value = subtract(previousValue, currentValue);
     }
     else{
-        currentValue = add(previousValue, currentValue);
+        calcScreen.value = add(previousValue, currentValue);
     }
 
-    
-    calcScreen.value = currentValue;
+    /*if (currentOperator == "divide"){
+        let scooch = currentValue;
+        currentValue = previousValue;
+    }*/
     operated = true;
-    newInput= false;
 
 }
 
 
 const equalsButton = document.querySelector("#equals-button");
-equalsButton.addEventListener("click", operate);
+equalsButton.addEventListener("click", () => {
 
-/*const clearButton = document.getElementById("clear-button");
+    if (currentOperator != null){
+        //equals needs to call assignOperator rather than operate as the logic to determine which operator to use exists in that function
+        assignOperator(currentOperator, true);
+    }
+    else return;
+    
+});
+
+const clearButton = document.getElementById("clear-button");
 clearButton.addEventListener("click", () => {
 
-    operator = "";
-    inOperation= false;
+    previousOperator = null;
+    currentOperator = null;
+    operated = false;
+    newInput = false;
+
     currentValue = null;
+    previousValue = null;
+    
     calcScreen.value = null;
 
-})*/
+});
 
 
